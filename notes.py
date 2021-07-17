6f86a390e20a5195d8604820ad27cf9bbfc994f1
reillyn51 <64506642+reillyn51@users.noreply.github.com>
2021-07-17 10:42:26 -0400

    visualized data

--- notes.py
+++ notes.py
@@ -13,3 +13,54 @@
 ## Compile the model 
 ## Train the model 
 # Evaluation and Predictions 
+import os 
+import nibabel
+import imageio 
+
+cwd = os.getcwd()
+
+file_path = os.path.join(cwd, 'test_files')
+
+new_dirs = [os.path.join(file_path, 'brain_flair'),
+                os.path.join(file_path, 'brain_t1'),
+                os.path.join(file_path, 'brain_t1ce'),
+                os.path.join(file_path, 'seg'),
+                os.path.join(file_path, 'brain_t2')]
+
+for dir in new_dirs:
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+for file in os.listdir(file_path):
+    
+    
+    file_array = os.path.join(file_path, file)
+    if os.path.isfile(file_array) and file == "00000534_brain_t2.nii":
+        file_array = nibabel.load(file_array).get_fdata()
+
+        print("Loading ", file)
+        for i in range(file_array.shape[2]):
+            if 'brain_flair' in str(os.listdir(file_path)):
+                print('Saving to flair folder')
+                s = file_array[:,:,i]
+                imageio.imwrite(os.path.join(new_dirs[0], 'slice_{}.png'.format(i)), s)
+
+            if 'brain_t1' in str(os.listdir(file_path)):
+                print('Saving to t1 folder')
+                s = file_array[:,:,i]
+                imageio.imwrite(os.path.join(new_dirs[1], 'slice_{}.png'.format(i)), s)
+
+            if 'brain_t1ce' in str(os.listdir(file_path)):
+                print('Saving to t1ce folder')
+                s = file_array[:,:,i]
+                imageio.imwrite(os.path.join(new_dirs[2], 'slice_{}.png'.format(i)), s)
+
+            if 'seg' in str(os.listdir(file_path)):
+                print('Saving to seg folder')
+                s = file_array[:,:,i]
+                imageio.imwrite(os.path.join(new_dirs[3], 'slice_{}.png'.format(i)), s) 
+
+            if 'brain_t2' in str(os.listdir(file_path)):
+                print('Saving to t2 folder')
+                s = file_array[:,:,i]
+                imageio.imwrite(os.path.join(new_dirs[4], 'slice_{}.png'.format(i)), s) 
